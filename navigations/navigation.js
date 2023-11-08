b38d3785d9dfb08bc3ffd719b79b7c104c6c3126
Muhammad Hamza <123671986+mhamzashabbir@users.noreply.github.com>
2023-11-09 01:09:20 +0500

    B3: Navigation Setup (#4)
    * Splash Screen
    
    * B3: Navigation Setup

---
+++ navigations/navigation.js
@@ -0,0 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import GetStarted from './screens/GetStarted';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="GetStarted">
+      <Stack.Screen
+        name="GetStarted"
+        component={GetStarted}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
