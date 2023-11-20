b2c830089ca3ca979bc989fdb645b753fd19181d
Muhammad Hamza <mhamzashabbirs@gmail.com>
2023-11-20 17:38:06 +0500

    Input Color Changes

--- screens/auth/Login.js
+++ screens/auth/Login.js
@@ -133,6 +133,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.inputBackground,
     borderRadius: 10,
     marginTop: 10,
+    borderWidth: 1,
   },
   checkboxContainer: {
     flexDirection: 'row',
